16b27dac409c071c50267a8e0fee8325cfc6fab1
sarah <sarah.badr@epfl.ch>
2025-05-07 20:52:41 +0200

    feat: Docker and build

--- projects/libpng/build.sh
+++ projects/libpng/build.sh
@@ -26,13 +26,12 @@
 cat scripts/pnglibconf.dfa | \
   sed -e "s/option STDIO/option STDIO disabled/" \
       -e "s/option WARNING /option WARNING disabled/" \
-      -e "s/option WRITE enables WRITE_INT_FUNCTIONS/option WRITE disabled/" \
 > scripts/pnglibconf.dfa.temp
 mv scripts/pnglibconf.dfa.temp scripts/pnglibconf.dfa
 
 # build the libpng library.
 autoreconf -f -i
-./configure --with-libpng-prefix=OSS_FUZZ_
+./configure 
 make -j$(nproc) clean
 make -j$(nproc) libpng16.la
 
