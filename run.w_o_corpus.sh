7639e349931116860a3e65f289cdc2fd94ec5db6
johannayara <johanna.jacomenoianuding@epfl.ch>
2025-05-11 15:10:34 +0200

    Revert "refactor repo"
    This reverts commit 06bc94110505cefbd36e324b30932c6d05f78012.

--- run.w_o_corpus.sh
+++ run.w_o_corpus.sh
@@ -1,12 +1,13 @@
-
 #!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-mkdir -p build/out/w_new_corpus
-if [ -z "$(ls -A build/out/w_new_corpus)" ]; then
-    cp -a build/out/libpng/test_corpus/. build/out/w_new_corpus
+# Remove existing seed corpus if it exists
+SEED_CORPUS="build/out/libpng/libpng_read_fuzzer_seed_corpus.zip"
+if [ -f "$SEED_CORPUS" ]; then
+  echo "Removing existing seed corpus: $SEED_CORPUS"
+  rm "$SEED_CORPUS"
 fi
 
 # Apply the diff file
@@ -24,11 +25,20 @@ else
 fi
 
 
+NAME="${1:-}"
+DIR="build/out/${NAME}w_o_corpus"
+
+echo "Created directory: $DIR"
+
+
 # Build the Docker image for libpng
 python3 infra/helper.py build_image libpng
 
 # Build the fuzzers for libpng
 python3 infra/helper.py build_fuzzers libpng
 
+# Create the output corpus directory
+mkdir -p "$DIR"
+
 # Run the fuzzer
-python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/w_new_corpus
+python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir "$DIR"
