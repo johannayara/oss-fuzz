97a72d3afec96573e939cd76fdb9bbf53f9d9ce6
johannayara <johanna.jacomenoianuding@epfl.ch>
2025-05-11 16:10:30 +0200

    refactor repo

--- part1/report/run.w_o_corpus.sh
+++ part1/report/run.w_o_corpus.sh
@@ -32,13 +32,13 @@ echo "Created directory: $DIR"
 
 
 # Build the Docker image for libpng
-python3 infra/helper.py build_image libpng
+python3 ../../infra/helper.py build_image libpng
 
 # Build the fuzzers for libpng
-python3 infra/helper.py build_fuzzers libpng
+python3 ../../infra/helper.py build_fuzzers libpng
 
 # Create the output corpus directory
-mkdir -p "$DIR"
+mkdir -p "../../${DIR}"
 
 # Run the fuzzer
-python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir "$DIR"
+python3 ../../infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir "$DIR"
