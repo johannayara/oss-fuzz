e1f8e32f4ecb3b5f35b003f963dcb9df739e7fab
johannayara <johanna.jacomenoianuding@epfl.ch>
2025-05-14 13:55:15 +0200

    add corpus

--- projects/libpng/build.sh
+++ projects/libpng/build.sh
@@ -45,7 +45,7 @@ $CXX $CXXFLAGS -std=c++11 -g -I. \
 
 # add seed corpus.
 find $SRC/libpng -name "*.png" | grep -v crashers | \
-     xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
+     xargs zip $OUT/libpng_write_fuzzer_seed_corpus.zip
 
 cp $SRC/libpng/contrib/oss-fuzz/*.dict \
      $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
